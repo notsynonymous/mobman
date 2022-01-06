9aa770d9aa57edfd12f82fcab2f88b163cc820e3
notsynonymous <89937869+notsynonymous@users.noreply.github.com>
2022-01-06 07:27:12 -0700

    Add files via upload

---
+++ R/testfunction.R
@@ -0,0 +1,40 @@
+#' Testing dodcumentation on this package
+#' This is a program that says your vagina is nasty 9999 times
+#' @name testFunction
+#'
+#' @author Samuel MDC
+#'
+#' @param name an object of type string that takes a name and completes a sentence about how nasty your vagina is. If blank, name is substituted for the current username.
+#'
+#' @returns string_n
+#' 
+#' @examples
+#' V <- testFunction("Samuel")
+#' V
+#' V <- testFunction("")
+#' V
+#' @export
+#' @md
+
+
+testFunction <- function(name) {
+
+	if(length(name) != 0){
+		string2 <- paste0("Hello, ", name)
+		string3 <- ", I think that your vagina is nasty"
+		string_n <- paste0(string2, string3)
+	} else {
+		string2 <- paste0("Hello, ", Sys.getenv("%USERNAME%"))
+		string3 <- ", I think that your vagina is nasty."
+		string_n <- paste0(string2, string3)
+	}
+
+	for(i in 1:99) {
+		print(string_n)
+		i <- i+1
+	}
+
+	print("Compiler works!")
+
+return(string_n)
+}
