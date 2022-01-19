c4c215b611a834af05cc63c40a963a8ce789466f
notsynonymous <89937869+notsynonymous@users.noreply.github.com>
2022-01-19 12:03:30 -0700

    Add files via upload

--- R/mntcapp.R
+++ R/mntcapp.R
@@ -1,6 +1,6 @@
 #' Bootstrap for App for Form Generation
 #' A generic inspection form (not technically Amazon affiliated)
-#' @name mntcapp
+#' @name mntcapp_beta
 #'
 #' @author Samuel Mark DeClerk
 #'
@@ -84,7 +84,7 @@ mntcapp <- function(){
     master_sublist.init <- list("Lights or light covers that are cracked, broken, or missing", "Turn signal that is not working", "Hazard light that is not working", "Battery boxes that are unsecured or damaged", "loose, hanging, faulty, or frayed wires", "Orange wires that are visible",  "Mirror or glass that is missing", "Side mirrors have cracks or damage that that is in your line of sight", "Side mirrors that are loose, hanging, unsecured, or held up with a zip-tie, tape, or similar", "Tire that is flat, leaking air, bulging, or swelling", "Tire has objects, cuts, dents, or exposed wire on its surface", "Tire has insufficient tread (Less than 2/32 inch or 1.6mm)", "Tire has insufficient tread (Less than 4/32 inch or 3.2mm)", "Tire has uneven wear on treads", "Wheel, rim, or mounting equipment that is damaged, cracked, or broken ", "Wheel nuts that are missing, or loose", "Tire that is flat, leaking air, bulging, or swelling", "Tire has objects, cuts, dents, or exposed wire on its surface", "Tire has insufficient tread (Less than 2/32 incj or 1.6mm)", "Tire has uneven wear on treads", "Wheel, rim, or mounting equipment that is damaged, cracked, or broken", "Mud Flap that is damaged, missing, unsecured or held up with a zip-tie, tape or similar", "Wheel nuts that are missing, or loose", "Charging port cap that is missing or broken", "Active red, orange, yellow, green, blue, brown, black, or pink fluid leaking on the ground", "Items attached to the body of the vehicle (for example: lift gates and hood latches) that are missing, damaged, loose, unsecure, hanging, or held with a zip-tie, tape, or similar", "Rear step/bumper that is damaged/loose/unsecure/hanging/missing (Branded Vehicles Only)", "Side step that is damaged/loose/unsecure/hanging/missing (Branded Vehicles Only)", "Doors cannot open, or close", "License plates/temp tags/ that are damaged, missing, or expired", "Insurance Information that is missing or expired", "Short Haul Exemption that is missing or expired", "Vehicle Registration that is missing or expired ", "Certification of Lease that is missing", "Repair documentation audit failure", "DOT/CA BIT/ State Inspection sticker that is missing or expired", "Seatbelt that is missing, torn, or frayed", "Seatbelt that is not working", "Wipers that are missing, damaged, or not working", "Windshield has crack/chips/stars/bullseyes or damage that that is in your line of sight", "Windshield has crack/chips/stars/bullseyes or damage that that is more than 1 inch", "Device/Accessory that is mounted on the windshield", "Fire extinguisher that is missing, mounted with a tape, zip-tie or similar, or the dial/needle that is not in the green zone", "Delivery device cradle that is damaged, missing, or that is mounted with a tape, zip-tie or similar", "Reflective triangles that are missing", "Spare fuses that are missing", "Camera monitor that is missing, broken, or not working ", "Camera monitor that is mounted with a tape, zip-tie, or similar", "Back-up camera that is loose, hanging, missing, or mounted with a tape, zip-tie, or similar", "Netradyne camera that is hanging/disconnected from bracket", "Medical card that is not present, or that is expired", "PM that is overdue", "Recall/Campaign that is overdue", "Exterior of vehicle that is visably dirty when viewed from 20 feet away", "Amazon Prime decal that is damaged, missing, or excessively dirty", "Interior of vehicle has excessive grim, dust, or trash present")
     severity <- list("1", "2")
     grounded <- list("Grounded", "Active")
-    vehicleType <- list("All Types", "Electric", "Regulated", "Commercial Van", "Step Van", "Non CV, SV, or EV")
+    vehicleType <- list("All Types", "Electric", "Regulated", "Commercial Van", "Step Van", "Non CV, SV, or EV") #select one or more
     modelNumber <- list("NRR DSL REG, 195, 268A, MT45G, E-450 STRIPPED, F-59 COMMERCIAL STRIPPED", "NRR DSL REG, 195, 268A, MT45G, E-450 STRIPPED, F-59 COMMERCIAL STRIPPED, V8100, TRANSIT-350HD, SV01, F59", "Other not listed") #select most applicable
     NAOnly <- list("All", "NA", "Don't know")
 
@@ -104,30 +104,30 @@ mntcapp <- function(){
     ),
     sidebarLayout(
         sidebarPanel(
-        textInput("vin_numnbers", "Enter the VIN numbers seperated by a comma: ", "VIN NUMBERS"),
-        selectInput("category.main", "Main Damage Categories", choices = master_main.init),       
-        selectInput("category.sub", "Damage Subcategories", choices = master_sublist.init),          
-        selectInput("severity", "Level of Severity", choices = severity),                           
-        selectInput("grounded", "Grounding Status", choices = grounded),                             
-        selectInput("vehicleType", "Type of Vehicle Being Serviced", choices = vehicleType),         
-        selectInput("modelNumber", "Model Number (select most applicable)", choices = modelNumber)   
+        textInput("vinnumber", "VIN-Numbers", "placeholder"),
+        selectInput("category.main", "Main Damage Categories", choices = master_main.init),
+        selectInput("category.sub", "Damage Subcategories", choices = master_sublist.init),
+        selectInput("severity", "Level of Severity", choices = severity),
+        selectInput("grounded", "Grounding Status", choices = grounded),
+        selectInput("vehicleType", "Type of Vehicle Being Serviced", choices = vehicleType),
+        selectInput("modelNumber", "Model Number (select most applicable)", choices = modelNumber)
         ),
         mainPanel(
-          tags$h4("Generated Form: "),
-          tags$h2(htmlOutput("concatForm"))
+        tags$h4("Generated Form: "),
+        tags$h2(htmlOutput("concatForm"))
         )
     )
     )
 
     server <- function(input, output, session) {
-
+   
         output$concatForm <- renderUI({
-            str0 <- paste0(input$vin_numbers, " have ")
+            str0 <- paste0(input$vinnumber, " have ")
             str1 <- paste0(str0, tolower(input$category.sub))
             if(endsWith(input$category.main, 's')){
                 str2 <- paste(" for the ", tolower(input$category.main), ".", sep="")    
             } else {
-                str2 <- paste(" for the ", tolower(input$category.main), ".", sep="")    
+                str2 <- paste(" for the ", tolower(input$category.main), ".")    
             }
             
 
@@ -156,5 +156,6 @@ mntcapp <- function(){
 
     }
 
-    shinyApp(ui, server)
+shinyApp(ui, server)
+
 }
